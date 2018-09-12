f5105dbd0db66845de695ab8e0af2ff5ca52acb8
faustian <icfaust@gmail.com>
2018-09-12 15:12:49 +0200

    adding solutions for problem 3

--- hw1/motion_command.py
+++ hw1/motion_command.py
@@ -41,17 +41,28 @@ def v2t(p):
         p (3x1 numpy array): the robot pose in cartesian [x, y, theta]
              
     Returns:
-        h (3x1 numpy array): the robot pose in homogeneous [x, y, theta]
+        M (3x3 numpy array): the robot pose in homogeneous [[R, t],[0, 0, 1]]
     """
+    theta = p[2]
+    
+    if not theta: #divide by zero avoidance
+        theta = 1.
+            
+    output = scipy.array([[scipy.cos(p[2]), -scipy.sin(p[2]), p[0]/theta],
+                          [scipy.sin(p[2]), scipy.cos(p[2]), p[1]/theta],
+                          [0., 0., 1.]])
+    
+    return output
+    
 
 def t2v(R):
     
     """ Transforms cartesian coordinates into homogeneous coordinates
    
     Args:
-        h (3x1 numpy array): the robot pose in homogeneous [x, y, theta]
+        M (3x1 numpy array): the robot pose in homogeneous [[R, t],[0, 0, 1]]
              
     Returns:
         p (3x1 numpy array): the robot pose in cartesian [x, y, theta]
     """
-    pass    
+    return scipy.squeeze(scipy.arctan2(R[1,0],R[0,0])*R[:,2])
