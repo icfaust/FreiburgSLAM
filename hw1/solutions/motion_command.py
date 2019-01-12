8547a93196f30d67b0aa5e46d25469967846cb84
faustian <icfaust@gmail.com>
2019-01-12 11:12:14 +0100

    motion command solution separated from problem set

--- hw1/solutions/motion_command.py
+++ hw1/solutions/motion_command.py
@@ -23,9 +23,11 @@ def motion_command(x, u):
      """
 
     #TODO: update x according to the motion represented by u
-
     
+    x[0] += u['t']*scipy.cos(x[2] + u['r1'])
+    x[1] += u['t']*scipy.sin(x[2] + u['r1'])
     #TODO: remember to normalize theta by calling normalize_angle for x[2]
+    x[2] = (((x[2] + u['r1'] + u['r2'])/scipy.pi + 1.) % 2. - 1.)*scipy.pi
     
     return x
 
@@ -42,7 +44,16 @@ def v2t(p):
     Returns:
         M (3x3 numpy array): the robot pose in homogeneous [[R, t],[0, 0, 1]]
     """
-    pass
+    theta = p[2]
+    
+    if not theta: #divide by zero avoidance
+        theta = 1.
+            
+    output = scipy.array([[scipy.cos(p[2]), -scipy.sin(p[2]), p[0]/theta],
+                          [scipy.sin(p[2]), scipy.cos(p[2]), p[1]/theta],
+                          [0., 0., 1.]])
+    
+    return output
     
 
 def t2v(R):
@@ -55,5 +66,15 @@ def t2v(R):
     Returns:
         p (3x1 numpy array): the robot pose in cartesian [x, y, theta]
     """
-    pass
+    return scipy.squeeze(scipy.arctan2(R[1,0],R[0,0])*R[:,2])
 
+# Answer b) : the difference p2-p1 can be transformed into a homogeneous
+# transform using delta x, delta y and delta theta
+
+# Answer c): the answer to b can be recast to answer this problem. Relative
+# pose means that R is essentially the identity matrix in the homogenous
+# transform and z=t. This the homogeneous transform is fully specified.  Thus:
+#
+#  [[0,1,2/pi],     [[4/pi],    [[2/pi],                  [[1],
+#   [1,0,2/pi],  x   [0],    =   [6/pi], == (in (x,y))==   [3]]
+#   [0,0,1]]         [1]]        [1]]
