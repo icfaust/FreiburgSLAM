nearly complete, there is some weighting bug

hw8/hw8.py
@@ -24,6 +24,7 @@ data = main.read_data('../sensor_data.dat')
 n = len(landmarks['id'])
 
 noise = scipy.array([0.005, 0.01, 0.005])
+#noise = scipy.zeros((3,))
 
 # how many particles
 numParticles = 100
@@ -34,7 +35,7 @@ particles = [{'weight':1./numParticles,
               'pose':scipy.zeros((3,)),
               'history':[],
               'landmarks':[{'observed':False,
-                            'mu':scipy.zeros((2,1)),
+                            'mu':scipy.zeros((2,)),
                             'sigma':scipy.zeros((2,2))} for i in range(n)]} for x in range(numParticles)]
 
 # toogle the visualization type
@@ -44,7 +45,7 @@ showGui = True  # show a window while the algorithm runs
 # Perform filter update for each odometry-observation pair read from the
 # data file.
 for t in range(len(data['odometry'])):
-#for t in range(50):
+#for t in range(2):
     print('timestep = %d' % t)
 
     # Perform the prediction step of the particle filter

hw8/main.py
@@ -42,15 +42,14 @@ def resample(particles):
 
     # walk along the wheel to select the particles
     for i in range(numParticles):# 1:numParticles
-        position += step;
-        if position > weightSum: #Is this necessary???
-            position -= weightSum; #I have a feeling this was
-                   #dubiously programmed...
+        position += step
+        if position > weightSum:
+            position -= weightSum 
             idx = 0
         while position > cs[idx]:
             idx += 1
 
-        newParticles[i] = particles[idx]
+        newParticles[i] = particles[idx].copy()
         newParticles[i]['weight'] = 1./numParticles
 
     return newParticles
@@ -74,7 +73,6 @@ def measurement_model(particle, z):
                                                     landmarkX - particle['pose'][0]) - particle['pose'][2])
 
     h = scipy.array([expectedRange, expectedBearing])
-    
     # TODO: Compute the Jacobian H of the measurement function h wrt the landmark location
     H = scipy.zeros((2,2))
 
@@ -118,7 +116,7 @@ def prediction(particles, u, noise):
     for i in range(numParticles):#1:numParticles
 
         # append the old position to the history of the particle
-        particles[i]['history'] += [particles[i]['pose']]
+        particles[i]['history'] += [particles[i]['pose'].copy()]
         
         # sample a new pose for the particle
         r1 = scipy.stats.norm.rvs(loc=u['r1'], scale=r1Noise)

hw8/plot.py
@@ -51,7 +51,7 @@ def plot_state(particles, landmarks, timestep, z, window=True):
                  linewidth=1.)
         
     # draw the trajectory as estimated by the currently best particle
-    trajectory = scipy.atleast_2d(scipy.hstack(particles[idx]['history']))
+    trajectory = scipy.atleast_2d(scipy.vstack(particles[idx]['history']))
     plt.plot(trajectory[:,0], trajectory[:,1], color='r', linewidth=3.)
 
     drawrobot(particles[idx]['pose'], 'r', 3, 0.3, 0.3)

hw8/solutions/fastslam.py
@@ -1,5 +1,6 @@
 import scipy
 import scipy.stats
+import scipy.linalg
 import main
 
 def correction(particles, z):
@@ -33,7 +34,7 @@ def correction(particles, z):
 
             # If the landmark is observed for the first time:
             
-            if particles[i]['landmarks'][j]['observed'] == False:
+            if particles[i]['landmarks'][l]['observed'] == False:
 
                 # TODO: Initialize its position based on the measurement and the current robot pose:
             
@@ -44,7 +45,7 @@ def correction(particles, z):
                 # TODO: initialize the EKF for this landmark
 
                 # Indicate that this landmark has been observed
-                particles[i]['landmarks'][j]['observed'] = True
+                particles[i]['landmarks'][l]['observed'] = True
                 
             else:
 